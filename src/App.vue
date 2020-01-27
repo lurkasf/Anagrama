287f5252193e6a05b52115f33acf46865a631da0
lurkasf <ferrariluska@gmail.com>
2020-01-27 11:45:55 -0400

    Atualização hehe

--- src/App.vue
+++ src/App.vue
@@ -8,15 +8,15 @@
       <div class="container">
       <div class="row">
         
-        <div class="col s4 offset-s4" >
+        <div class="col s12 m6 offset-m3" >
           <div id="cardGame" class="card-panel hoverable center">
             <form @submit.prevent="confirmaTentativa">
-                <label><h5 class="capitalizar negrito" >{{palavraSave}}</h5></label>
-                <input class="capitalizar" type="text" placeholder="Palavra" v-model="palavra">
-                <button class="waves-effect waves-light btn-small">Salvar<i class="material-icons left">save</i></button>
+                <label><h5 class="capitalizar negrito" >{{palavraEmbaralhadaSave}}</h5></label>
+                <input id="inputTentativa" class="capitalizar" type="text" placeholder="inserir palavra" v-model="palavra">
+                <button class="waves-effect waves-light btn-small salvarPalavra">Salvar<i class="material-icons left">save</i></button>
             </form>
             <!-- <button @click="embaralha()" class="waves-effect waves-light btn-small">Embaralha</button> -->
-            <button @click="novaPalavra()" class="waves-effect waves-light btn-small">Nova Palavra<i class="material-icons left">shuffle</i></button>
+            <button @click="novaPalavra()" class="waves-effect waves-light btn-small novaPalavra">Nova Palavra<i class="material-icons left">shuffle</i></button>
           </div>
         </div>
 
@@ -54,15 +54,16 @@
 </template>
 
 <script>
-  import words from './assets/words.json'
+  import words from './assets/words.json' //arquivo que contêm todas as palavras que rodam no programa
   export default{
     data(){
       return{
-        titulo : 'A N A G R A M A',
-        palavra : 'anagrama',
-        palavraSave : 'gamarana',
+        titulo : 'A N A G R A M A', //titulo que fica na barra superior
+        palavra : 'anagrama', //palavra atualmente escrita no input, muda de acordo com o que o user escreve
+        palavraEmbaralhadaSave : 'gamarana', //salva a palavra embaralhada para não perder quando editar no input do usuario
         originalWord : 'anagrama',
-        palavras : words["words"],
+        palavras : words["words"], //salva em 'palavras' todas as palavras contidas no arquivo de palavras (words.json)
+        get console() { return window.console; } //pegar o console pra poder usar, sem isso ele não conhece console
       }
     },
     mounted(){
@@ -70,23 +71,26 @@
     },
     methods:{
       confirmaTentativa(){
-        if(this.palavra.toLowerCase().trim() == this.originalWord){
+        if(this.palavra === ''){
+          this.console.log('%c Nenhuma Palavra Enviada!', 'color: orange;font-weight:bold');
+          this.setAlertStyle()
+        }else if(this.palavra.toLowerCase().trim() === this.originalWord){
           //alert("Acertou!")
-          this.setNewClass("cardGame", "vitoria")
-          this.removeClass("cardGame", "derrota")
-          this.setNewClass("fogos", "fireworks")
-        }else{
+          this.console.log('%c Tentativa Correta!', 'color: green;font-weight:bold');
+          this.limpaStyle()
+          this.setWinStyle()
+        } else{
           //alert("Errou!")
-          this.setNewClass("cardGame", "derrota")
-          this.removeClass("cardGame", "vitoria")
-          this.removeClass("fogos", "fireworks")
+          this.console.log('%c Tentativa Incorreta', 'color: red;font-weight:bold');
+          this.limpaStyle()
+          this.setLoseStyle()
         }
       },
 
       novaPalavra(){
         this.pickWord()
         this.embaralha()
-        this.palavraSave = this.palavra
+        this.limpaStyle() 
       },
 
       ///metodo para embaralhar a palavra original
@@ -96,23 +100,43 @@
         if(this.palavra == this.originalWord){
           this.embaralha()
         }
+        this.palavraEmbaralhadaSave = this.palavra // salva a palavra embaralhada para não perder
       },
       pickWord(){
         var randnum = Math.floor(Math.random() * this.palavras.length);
         this.originalWord = this.palavras[randnum]
       },
-      setNewClass(itemId,novaClasse){
+      setNewClass(itemId,novaClasse){ //metodo que pode adicionar uma classe de um elemento
         var element = document.getElementById(itemId);
         element.classList.add(novaClasse);
       },
-      removeClass(itemId, classe){
+      removeClass(itemId, classe){ //classe que pode remover uma classe de um elemento
         var element = document.getElementById(itemId);
         element.classList.remove(classe);
+      },
+      limpaStyle(){ //usada para remover todo o estilo dinâmico para vitorias e derrotas do #cardGame
+        this.removeClass("cardGame", "vitoria")
+        this.removeClass("cardGame", "derrota")
+        this.removeClass("cardGame", "alert")
+        this.removeClass("inputTentativa", "alert")
+        this.removeClass("fogos", "fireworks")
+      },
+      setWinStyle(){ //Modifica o estilo dos elementos para vitória
+        this.setNewClass("cardGame", "vitoria")
+        this.setNewClass("fogos", "fireworks")
+      },
+      setLoseStyle(){ //Modifica o estilo dos elementos para derrota
+        this.setNewClass("cardGame", "derrota")
+      },
+      setAlertStyle(){
+        this.setNewClass("cardGame", "alert")
+        this.setNewClass("inputTentativa", "alert")
       }
     },   
   }
 //opções de icone: shuffle, loop, autorenew
 </script>
+
 <style scoped>
   .capitalizar{
     text-transform: capitalize
@@ -130,8 +154,21 @@
   .derrota{
     background-color: rgb(255, 121, 121)
   }
+  .alert{
+    background-color: rgb(255, 201, 121)
+  }
+  input.alert::placeholder{
+    color: red;
+  }
   .negrito{
     color: black;
     font-weight: bold;
+  }
+  ::placeholder{
+    color: rgb(65, 65, 65);
+  }
+  button{
+    min-width: 92px;
+    max-width: 135px;
   }
 </style>
